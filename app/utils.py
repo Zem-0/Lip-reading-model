8f61c07e16d5214f729cb9dd623c708a0e782b07
Zem-0 <154821647+Zem-0@users.noreply.github.com>
2024-07-09 13:06:54 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,48 +1,58 @@
-import tensorflow as tf
-from typing import List
-import cv2
+
+# Import all of the dependencies
+import streamlit as st
 import os 
+import imageio 
 
-vocab = [x for x in "abcdefghijklmnopqrstuvwxyz'?!123456789 "]
-char_to_num = tf.keras.layers.StringLookup(vocabulary=vocab, oov_token="")
-# Mapping integers back to original characters
-num_to_char = tf.keras.layers.StringLookup(
-    vocabulary=char_to_num.get_vocabulary(), oov_token="", invert=True
-)
+import tensorflow as tf 
+from utils import load_data, num_to_char
+from modelutil import load_model
 
-def load_video(path:str) -> List[float]: 
-    #print(path)
-    cap = cv2.VideoCapture(path)
-    frames = []
-    for _ in range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))): 
-        ret, frame = cap.read()
-        frame = tf.image.rgb_to_grayscale(frame)
-        frames.append(frame[190:236,80:220,:])
-    cap.release()
-    
-    mean = tf.math.reduce_mean(frames)
-    std = tf.math.reduce_std(tf.cast(frames, tf.float32))
-    return tf.cast((frames - mean), tf.float32) / std
-    
-def load_alignments(path:str) -> List[str]: 
-    #print(path)
-    with open(path, 'r') as f: 
-        lines = f.readlines() 
-    tokens = []
-    for line in lines:
-        line = line.split()
-        if line[2] != 'sil': 
-            tokens = [*tokens,' ',line[2]]
-    return char_to_num(tf.reshape(tf.strings.unicode_split(tokens, input_encoding='UTF-8'), (-1)))[1:]
+# Set the layout to the streamlit app as wide 
+st.set_page_config(layout='wide')
 
-def load_data(path: str): 
-    path = bytes.decode(path.numpy())
-    #file_name = path.split('/')[-1].split('.')[0]
-    # File name splitting for windows
-    file_name = path.split('\\')[-1].split('.')[0]
-    video_path = os.path.join('.','data','s1',f'{file_name}.mpg')
-    alignment_path = os.path.join('.','data','alignments','s1',f'{file_name}.align')
-    frames = load_video(video_path) 
-    alignments = load_alignments(alignment_path)
-    
-    return frames, alignments
+# Setup the sidebar
+with st.sidebar: 
+    st.image('https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png')
+    st.title('LipBuddy')
+    st.info('This application is originally developed from the LipNet deep learning model.')
+
+st.title('LipNet Full Stack App') 
+# Generating a list of options or videos 
+options = os.listdir(os.path.join('..', 'data', 's1'))
+selected_video = st.selectbox('Choose video', options)
+
+# Generate two columns 
+col1, col2 = st.columns(2)
+
+if options: 
+
+    # Rendering the video 
+    with col1: 
+        st.info('The video below displays the converted video in mp4 format')
+        file_path = os.path.join('..','data','s1', selected_video)
+        os.system(f'ffmpeg -i {file_path} -vcodec libx264 test_video.mp4 -y')
+
+        # Rendering inside of the app
+        video = open('test_video.mp4', 'rb') 
+        video_bytes = video.read() 
+        st.video(video_bytes)
+
+
+    with col2: 
+        st.info('This is all the machine learning model sees when making a prediction')
+        video, annotations = load_data(tf.convert_to_tensor(file_path))
+        imageio.mimsave('animation.gif', video, fps=10)
+        st.image('animation.gif', width=400) 
+
+        st.info('This is the output of the machine learning model as tokens')
+        model = load_model()
+        yhat = model.predict(tf.expand_dims(video, axis=0))
+        decoder = tf.keras.backend.ctc_decode(yhat, [75], greedy=True)[0][0].numpy()
+        st.text(decoder)
+
+        # Convert prediction to text
+        st.info('Decode the raw tokens into words')
+        converted_prediction = tf.strings.reduce_join(num_to_char(decoder)).numpy().decode('utf-8')
+        st.text(converted_prediction)
+        

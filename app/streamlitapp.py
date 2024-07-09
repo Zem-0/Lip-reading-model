6feaf980fbb349ef2f3d898a31453528269eb221
Zem-0 <154821647+Zem-0@users.noreply.github.com>
2024-07-09 12:16:38 +0530

    Update streamlitapp.py

--- app/streamlitapp.py
+++ app/streamlitapp.py
@@ -8,7 +8,9 @@ import tensorflow as tf
 from modelutil import load_model
 
 
+vocab = [x for x in "abcdefghijklmnopqrstuvwxyz'?!123456789 "]
 char_to_num = tf.keras.layers.StringLookup(vocabulary=vocab, oov_token="")
+# Mapping integers back to original characters
 num_to_char = tf.keras.layers.StringLookup(
     vocabulary=char_to_num.get_vocabulary(), oov_token="", invert=True
 )
